3e6e1ffbaa6b8a57811b8bd34b8eac6aa4e297f3
Alexander Strong <alex.j.strong@gmail.com>
2017-02-26 11:19:03 -0500

    added LMGTFY links for career sites

--- sponsors.js
+++ sponsors.js
@@ -1,1 +1,14 @@
-sponsors = ["BAE", "Citi", "Viacom", "Vanguard", "HRT", "AIS", "Bloomberg", "Lockheed Martin", "GE", "PWC", "76West", "Rubenstein Technology Group"];
+sponsors = {
+    "BAE Systems": "http://lmgtfy.com/?q=BAE+systems+careers",
+    "Citi": "http://lmgtfy.com/?q=Citi+careers",
+    "Viacom": "http://lmgtfy.com/?q=Viacom+careers",
+    "Vanguard": "http://lmgtfy.com/?q=Vanguard+careers",
+    "Hudson River Trading": "http://lmgtfy.com/?q=Hudson+River+Trading+careers",
+    "AIS": "http://lmgtfy.com/?q=Assured+Information+Security+careers",
+    "Bloomberg": "http://lmgtfy.com/?q=Bloomberg+careers",
+    "Lockheed Martin": "http://lmgtfy.com/?q=Lockheed+Martin+careers",
+    "GE": "http://lmgtfy.com/?q=GE+careers",
+    "PWC": "http://lmgtfy.com/?q=PWC+careers",
+    "76West": "http://lmgtfy.com/?q=NYS+76West+careers",
+    "Rubenstein Technology Group": "http://lmgtfy.com/?q=Rubenstein+Tech+careers"
+}
